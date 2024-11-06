8e5c65e4bd21374032ad0aff6ea1701e899689d4
TC-HC <giddlfdka12345@gmail.com>
2024-11-07 08:30:29 +0900

    이게 왜 안됐냐 코드 추가함

--- R and E/Assets/ChainGenerator/Scripts/Pool/TeethPool.cs
+++ R and E/Assets/ChainGenerator/Scripts/Pool/TeethPool.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using GenericHelper;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 namespace Chain
@@ -8,18 +9,35 @@ namespace Chain
     [ExecuteInEditMode]
     public class TeethPool : Pool<Tooth>
     {
+        // TeethPool 클래스 내 ActivatePool에서 Collider 추가 확인
         public void ActivatePool()
         {
-            if (pool.Count > 0) return;
-            
-            var poolChildren = GetComponentsInChildren<Tooth>(true).ToList();
-            var childrenLength = poolChildren.Count;
+            // 먼저 모든 기존 Collider 삭제
+            foreach (var col in GetComponentsInChildren<BoxCollider>())
+            {
+                DestroyImmediate(col);
+            }
 
-            if (childrenLength > 0)
+            if (pool.Count > 0) return;
+
+            var poolChildren = GetComponentsInChildren<Tooth>(true).ToList();
+
+            foreach (var child in poolChildren)
+            {
+                // Collider가 없으면 추가
+                if (child.GetComponent<Collider>() == null)
+                {
+                    child.gameObject.AddComponent<BoxCollider>(); // 원하는 Collider로 대체 가능
+                }
+            }
+
+            if (poolChildren.Count > 0)
                 RestorePool(poolChildren.ToArray());
-            
         }
-        
+
+
+
+
         public void DeletePool()
         {
             DestroyImmediate(gameObject, true);
