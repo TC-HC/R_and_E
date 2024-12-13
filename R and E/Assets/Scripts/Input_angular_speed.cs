9ede0aaadc2d7738c5eee4e12d0b84addbb22a97
eunh07 <eunho070303@gmail.com>
2024-12-13 16:41:52 +0900

    inputfield

--- R and E/Assets/Scripts/Input_angular_speed.cs
+++ R and E/Assets/Scripts/Input_angular_speed.cs
@@ -1,10 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Input_angular_speed : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI text;
+
+    public void OnEndEditEvent(string str)
+    {
+        text.text = $"End Edit : {str}";
+    }
     // Start is called before the first frame update
     void Start()
     {
